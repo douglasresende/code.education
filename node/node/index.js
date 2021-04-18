9198e9c73a0069927037c5e1054b61448573e838
Douglas Camargo <douglas@maxstudio.com.br>
2021-04-18 19:42:18 -0300

    Adding html list database values with nodejs

--- node/node/index.js
+++ node/node/index.js
@@ -13,17 +13,23 @@ const connection = mysql.createConnection(config)
 const sql_create_table = `CREATE TABLE IF NOT EXISTS people(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL) ENGINE=INNODB;`
 connection.query(sql_create_table)
 
-const sql = `INSERT INTO people(name) values('Douglas');`
+const sql = `INSERT INTO people(name) VALUES ('Douglas'), ('Maria'), ('José');`
 connection.query(sql)
 
 app.get('/', (req,res) => {
-  const sql_select_last_user = `SELECT name FROM people ORDER BY id DESC LIMIT 1;`
-  connection.query(sql_select_last_user, function(err, result, fields){
+  const sql_select_last_user = `SELECT name FROM people;`
+  connection.query(sql_select_last_user, function(err, rows){
     if (err) throw err;
-    const nome_do_banco = result[0].name;
-    res.send('<h1>Full Cycle!</h1><br>' + nome_do_banco)
+    let html_result = '<h1>Full Cycle!</h1><br>'
+    html_result = html_result+'<ul>'
+    rows.forEach(function(value){
+      console.log(value.name)
+      html_result = html_result+'<li>'+value.name+'</li>'
+    })
+    html_result = html_result+'</ul>'
+    res.send(html_result)
   })
-  connection.end()
+  // connection.end()
 })
 
 app.listen(port, ()=> {
